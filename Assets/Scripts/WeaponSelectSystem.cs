04c6a6cfd5092a79d1471e16e864a94a93086811
Daniel Patel <dan.patel@icloud.com>
2026-07-24 08:15:13 +0100

    Make weapon switching animate in and out. Make weapon switching change the guns material

--- Assets/Scripts/WeaponSelectSystem.cs
+++ Assets/Scripts/WeaponSelectSystem.cs
@@ -4,16 +4,20 @@ public static class WeaponSelectSystem
 {
     public static void Update(ref Character character)
     {
-        if (character.weaponSelectInput < 0 || character.weaponSelectInput == character.currentSelectedWeapon ||
+        if (character.weaponSelectInput < 0 
+            || character.weaponSelectInput == character.currentSelectedWeapon 
             // cannot switch weapons whilst shooting
-            0 < character.shotCooldown)
+            || 0 < character.shotCooldown
+            // cannot switch if already switching.
+            || character.timeToSwapCountdown > 0f)
         {
             return;
         }
 
-        // Need to switch weapons
         character.currentSelectedWeapon = character.weaponSelectInput;
-        // cancel reload
+        character.timeToSwapCountdown = Character.TimeToSwap;
+
         character.reloadCountdown = 0f;
     }
+        
 }
